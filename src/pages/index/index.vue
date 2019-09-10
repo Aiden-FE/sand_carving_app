8b7ad661c707b8cdd98832dad99f712f714cc71c
xiaomin.jin <xiaomin.jin@linkedcare.cn>
2019-09-10 20:09:55 +0800

    test(test): test

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{title}}</text>
+			<text class="title">{{title}} qunqun</text>
 		</view>
 	</view>
 </template>
@@ -15,7 +15,27 @@
 			}
 		},
 		onLoad() {
-
+			// #ifdef APP-PLUS
+			// uni.showModal({
+			// 	title: 'test',
+			// 	content: `${plus.io.PRIVATE_WWW} | ${plus.io.PRIVATE_DOC} | ${plus.io.PUBLIC_DOCUMENTS} | ${plus.io.PUBLIC_DOWNLOADS}`,
+			// 	success: function (res) {
+			// 		if (res.confirm) {
+			// 			console.log('用户点击确定');
+			// 		} else if (res.cancel) {
+			// 			console.log('用户点击取消');
+			// 		}
+			// 	}
+			// })
+			// test
+			console.log('test')
+			plus.io.requestFileSystem(plus.io.PUBLIC_DOCUMENTS, fs => {
+				console.log(fs)
+			}, e => uni.showModal({
+				content: e.message,
+				showCancel: false
+			}))
+			// #endif
 		},
 		methods: {
 
